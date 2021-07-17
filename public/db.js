1459e4a5683352a8fa96888ab3d4d3d56fe6ac5c
Suraj Verma <surajverma@rentalcars.com>
2021-07-17 08:02:50 +0100

    check database function

--- public/db.js
+++ public/db.js
@@ -6,22 +6,20 @@ const indexedDB =
   window.msIndexedDB ||
   window.shimIndexedDB;
 
-let db;
-
 // Open a connection to the indexedDB
 const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = ({ target }) => {
-  db = target.result;
+  const db = target.result;
   db.createObjectStore("pending", { autoIncrement: true });
 };
 
 request.onsuccess = ({ target }) => {
-  db = target.result;
+  const db = target.result;
 
   // check if app is online before reading from DB
   if (navigator.onLine) {
-    checkDatabase();
+    checkDatabase(db);
   }
 };
 
@@ -32,4 +30,32 @@ request.onerror = ({ target }) => {
 // function to read from IndexedDB to get all transactions
 // if transactions are retrieved then they are posted to the DB
 // and IndexedDB is cleared
-function checkDatabase() {}
+function checkDatabase(db) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    // if not empty then bulk insert all transactions to remote DB
+    if (getAll.result.length) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          // delete all records from indexedDB if successful
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
